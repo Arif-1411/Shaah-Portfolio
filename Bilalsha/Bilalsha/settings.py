9e94e2c5d39e4d5ae25908fa6ed0a5889922c708
jumanath <jumanath1411@gmail.com>
2026-02-02 13:15:06 -0800

    ddddd

--- Bilalsha/Bilalsha/settings.py
+++ Bilalsha/Bilalsha/settings.py
@@ -128,9 +128,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-STATIC_URL = 'static/'
-MEDIA_URL = '/Home/static/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Home/static/media')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'Home/static']  # optional for dev
+STATIC_ROOT = BASE_DIR / 'staticfiles'         # for deployment
+
+# Media files (for uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Allowed hosts
+ALLOWED_HOSTS = ['Bilalshaah.pythonanywhere.com']
 
 
 
